a68a377806308a31bf27ee3a2a04489f2218af5c
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-16 13:37:57 -0400

    added models for post and comment

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe Post, :type => :model do
   it "has a valid factory" do
     expect(FactoryGirl.create(:post)).to be_valid
   end
-  it "is invalid without a link"
-  it "is invalid without a title"
+  it "is invalid without a link" do
+    expect(FactoryGirl.build(:post, link: nil)).not_to be_valid
+  end
+  it "is invalid without a title" do
+    expect(FactoryGirl.build(:post, title: nil)).not_to be_valid
+  end
   it "belongs to a User"
 end
