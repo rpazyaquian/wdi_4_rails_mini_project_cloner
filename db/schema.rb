366cb92ef5490c439c7514b6754e2958c07b1f27
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-15 20:01:21 -0400

    created Posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015234320) do
+ActiveRecord::Schema.define(version: 20141016000020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "link"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
