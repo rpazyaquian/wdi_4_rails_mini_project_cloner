1eaffc68270a030b2acdb36c836a776a84230a97
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-16 11:13:37 -0400

    updated schema, added user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe User do
+RSpec.describe User, :type => :model do
   it "has a valid factory"
   it "is invalid without an email"
   it "is invalid without a username"
