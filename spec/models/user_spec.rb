12a99287a07a4b78a8a9b7572f9c0c76b81c3707
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-16 09:52:01 -0400

    added models subdirectory, added User specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe User do
+  it "has a valid factory"
+  it "is invalid without an email"
+  it "is invalid without a username"
+  it "is invalid without a password"
+  it "is invalid without a password confirmation"
+  it "is invalid without a matching password and password confirmation"
+end
